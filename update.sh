6add54329d33d446fc2ef3f0aa095e9ad67547de
gerrylon <gerrylon@163.com>
2018-12-03 02:07:51 +0800

    update

---
+++ update.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: update.sh
+# Author: gerrylon
+# mail: gerrylon@163.com
+# Created Time: 2018-12-03 02:05:52
+#########################################################################
+#!/bin/bash
+
+git pull
+vim +BundleInstall! +BundleClean +q
